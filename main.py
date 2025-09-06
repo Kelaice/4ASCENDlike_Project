9141ddbea260c0564c37d3a5c982485a03b780d2
nek0name <ylinzixuan@qq.com>
2025-09-06 19:35:19 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 import pygame
-import board
-import background
+from board import *
+from title_page import*
+
+#-------------------------------------------Initialize-----------------------------------------------
+TITLE=0;MENU=1;PVP=2;PVE=3;TEACH=4
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+background_image = pygame.image.load('resource\\background.png')
+titleBackground_image = pygame.image.load('resource\\titleBackground.png')
+
+
+state=TITLE
+TITLE_FLAG=True;WEL_FLAG=True
+
+
+
+
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type==pygame.QUIT:
             running = False
 
-    background.drawBackGround(screen)
+    match state:
+        case 0:
+            if TITLE_FLAG:
+                TITLE_FLAG=drawTitlePage(screen)
+            WEL_FLAG=drawWelcome(screen,titleBackground_image,WEL_FLAG)
+            for event in pygame.event.get():
+                if event.type==pygame.MOUSEBUTTONDOWN:
+                    state = MENU
+        case 1:
+            screen.fill(0xffffff)
+            screen.blit(background_image, (0, 0))
+            drawUI(screen)
+            turn = checkMouse(screen)
+            setPiece(turn)
+            drawPiece(screen)
 
-    board.drawUI(screen)
-    turn = board.checkMouse(screen)
-    board.setPiece(turn)
-    board.drawPiece(screen)
-
-    pygame.display.flip()
-
+            pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
