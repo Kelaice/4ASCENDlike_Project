1e0dfb08d239101e07218995459f6b8106215dd8
kelaice <1928127609@qq.com>
2025-09-09 04:23:28 +0800

    Add files via upload

--- board.py
+++ board.py
@@ -1,66 +1,112 @@
+#负责棋盘显示
 import pygame
 
-Boardsize = 9
-
-XtoXstart = 220
-YtoXstart = 150
-XtoYstart = 180
-YtoYstart = 180
-
-Rowlenth = 440
-Collenth = 420
-Distance = 45
-Line_color = 0x838383
-
-CheckRidus = 10
-PieceRidus = 15
-All_Piece = []
-Piece_Color = 0
-
-LeftUP_Point = (461,210)
-RightDown_Point = (821,571)
-
-PlayerHead_Pos = (150,70)
-EnemyHead_Pos = (1100,70)
-
 class Board:
-    def drawUI(self,screen):
+    def __init__(self):
+        self.Boardsize = 9
+
+        self.XtoXstart = 220
+        self.YtoXstart = 150
+        self.XtoYstart = 180
+        self.YtoYstart = 180
+
+        self.Rowlenth = 440
+        self.Collenth = 420
+        self.Distance = 45
+        self.Line_color = 0x838383
+
+        self.CheckRidus = 10
+        self.PieceRidus = 15
+        self.All_Piece = []
+        self.Piece_Color = 0
+
+        self.Magic_plants = []
+        self.Magic_plant_Color = "green"
+
+        self.LeftUP_Point = (461,210)
+        self.RightDown_Point = (821,571)
+
+        self.PlayerHead_Pos = (150,70)
+        self.EnemyHead_Pos = (1100,70)
+
+        self.Lock = 0
+
+    def drawUI(self, screen, hp1=6, hp2=6, initial_hp1=6, initial_hp2=6):
         '''绘制棋盘及玩家状态'''
-        self.drawState(screen)
+        self.drawState(screen, hp1, hp2, initial_hp1, initial_hp2)
         self.drawBoard(screen)
 
     def drawBoard(self,screen):
         #绘制棋盘
         origin = pygame.Vector2(screen.get_width()/2,screen.get_height()/2)
-        xfirstLine_start = origin-pygame.Vector2(XtoXstart,YtoXstart)#横向绘制起点
-        yfirstLine_start = origin-pygame.Vector2(XtoYstart,YtoYstart)#纵向绘制起点
-        xlineLenth = pygame.Vector2(Rowlenth,0)
-        ylineLenth = pygame.Vector2(0,Collenth)
+        xfirstLine_start = origin-pygame.Vector2(self.XtoXstart,self.YtoXstart)#横向绘制起点
+        yfirstLine_start = origin-pygame.Vector2(self.XtoYstart,self.YtoYstart)#纵向绘制起点
+        xlineLenth = pygame.Vector2(self.Rowlenth,0)
+        ylineLenth = pygame.Vector2(0,self.Collenth)
         
-        for i in range(Boardsize):#横向棋盘线
-            cur_start = xfirstLine_start+pygame.Vector2(0,Distance*i)
-            pygame.draw.line(screen,Line_color,cur_start,cur_start+xlineLenth,4)
-        for i in range(Boardsize):#纵向棋盘线
-            cur_start = yfirstLine_start+pygame.Vector2(Distance*i,0)
-            pygame.draw.line(screen,Line_color,cur_start,cur_start+ylineLenth,4)
+        for i in range(self.Boardsize):#横向棋盘线
+            cur_start = xfirstLine_start+pygame.Vector2(0,self.Distance*i)
+            pygame.draw.line(screen,self.Line_color,cur_start,cur_start+xlineLenth,4)
+        for i in range(self.Boardsize):#纵向棋盘线
+            cur_start = yfirstLine_start+pygame.Vector2(self.Distance*i,0)
+            pygame.draw.line(screen,self.Line_color,cur_start,cur_start+ylineLenth,4)
 
-    def drawState(self,screen):
-        #玩家状态
+    def drawState(self,screen,hp1=6,hp2=6,initial_hp1=6,initial_hp2=6):
+        # 玩家头像
         player_image = pygame.image.load("resource\\player.png")
         enemy_image = pygame.image.load("resource\\enemy.png")
-        screen.blit(player_image,PlayerHead_Pos)
-        screen.blit(enemy_image,EnemyHead_Pos)
+        screen.blit(player_image,self.PlayerHead_Pos)
+        screen.blit(enemy_image,self.EnemyHead_Pos)
+        # 玩家1血量条（头像下方）
+        bar_width = 200
+        bar_height = 20
+        avatar_size = 50
+        player1_x, player1_y = self.PlayerHead_Pos
+        bar_y = player1_y + avatar_size + 10
+        bg_rect = pygame.Rect(player1_x, bar_y, bar_width, bar_height)
+        pygame.draw.rect(screen, (192,192,192), bg_rect)
+        pygame.draw.rect(screen, (0,0,0), bg_rect, 2)
+        if hp1 > 0:
+            fill_width = int((hp1 / initial_hp1) * bar_width)
+            fill_rect = pygame.Rect(player1_x, bar_y, fill_width, bar_height)
+            color = (0,255,0) if hp1 > 2 else (255,0,0)
+            pygame.draw.rect(screen, color, fill_rect)
+        for i in range(1, initial_hp1):
+            grid_x = player1_x + int((i / initial_hp1) * bar_width)
+            pygame.draw.line(screen, (0,0,0), (grid_x, bar_y), (grid_x, bar_y + bar_height), 1)
+        f = pygame.font.Font(None, 18)
+        hp_text = f.render(f"{hp1}/{initial_hp1}", True, (0,0,0))
+        text_rect = hp_text.get_rect(center=(player1_x + bar_width // 2, bar_y + bar_height + 15))
+        screen.blit(hp_text, text_rect)
+        # 玩家2血量条（头像下方）
+        player2_x, player2_y = self.EnemyHead_Pos
+        bar_y2 = player2_y + avatar_size + 10
+        bg_rect2 = pygame.Rect(player2_x, bar_y2, bar_width, bar_height)
+        pygame.draw.rect(screen, (192,192,192), bg_rect2)
+        pygame.draw.rect(screen, (0,0,0), bg_rect2, 2)
+        if hp2 > 0:
+            fill_width2 = int((hp2 / initial_hp2) * bar_width)
+            fill_rect2 = pygame.Rect(player2_x, bar_y2, fill_width2, bar_height)
+            color2 = (0,255,0) if hp2 > 2 else (255,0,0)
+            pygame.draw.rect(screen, color2, fill_rect2)
+        for i in range(1, initial_hp2):
+            grid_x2 = player2_x + int((i / initial_hp2) * bar_width)
+            pygame.draw.line(screen, (0,0,0), (grid_x2, bar_y2), (grid_x2, bar_y2 + bar_height), 1)
+        hp_text2 = f.render(f"{hp2}/{initial_hp2}", True, (0,0,0))
+        text_rect2 = hp_text2.get_rect(center=(player2_x + bar_width // 2, bar_y2 + bar_height + 15))
+        screen.blit(hp_text2, text_rect2)
+        # 图片资源留空，未来可替换血条为图片
 
     def findPos(self):
         '''定位到棋盘节点'''
         mousePos = pygame.mouse.get_pos()
 
-        for i in range(LeftUP_Point[0],RightDown_Point[0]+5,Distance):
-            for j in range(LeftUP_Point[1],RightDown_Point[1]+5,Distance):
-                L = i-CheckRidus
-                R = i+CheckRidus
-                U = j+CheckRidus
-                D = j-CheckRidus
+        for i in range(self.LeftUP_Point[0],self.RightDown_Point[0]+5,self.Distance):
+            for j in range(self.LeftUP_Point[1],self.RightDown_Point[1]+5,self.Distance):
+                L = i-self.CheckRidus
+                R = i+self.CheckRidus
+                U = j+self.CheckRidus
+                D = j-self.CheckRidus
                 if(mousePos[0]>L and mousePos[0]<R and mousePos[1]>D and mousePos[1]<U):
                     return pygame.Vector2(i,j)
         return pygame.Vector2(0,0)
@@ -68,38 +114,53 @@ class Board:
     def checkMouse(self,screen):
         '''检查鼠标位置是否合法'''
         piecePos = self.findPos()
-        if piecePos[0]!=0:
-            pygame.draw.rect(screen,0x838383,[piecePos[0]-CheckRidus,piecePos[1]-CheckRidus,
-                                            CheckRidus*2,CheckRidus*2],1,1)
+        if piecePos[0]!=0 and self.checkPiece(piecePos):
+            pygame.draw.rect(screen,self.Line_color,[piecePos[0]-self.CheckRidus,piecePos[1]-self.CheckRidus,
+                                            self.CheckRidus*2,self.CheckRidus*2],1,1)
             return True
         return False
 
     def checkPiece(self,p:pygame.Vector2):
-        #检查当前位置是否已经有棋子
-        for i in All_Piece:
+        '''检查当前位置是否已经有棋子'''
+        for i in self.All_Piece:
             if(i[0][0]==p[0] and i[0][1]==p[1]):
                 return False
         return True
 
-    lock = 0
-    def setPiece(self,turn):
-        '''在合法位置落一个子'''
-        global Piece_Color
-        global lock
-
-        pressed = pygame.mouse.get_pressed()
-        piecePos = self.findPos()
-
-        if turn and not lock:
-            if pressed[0] and self.checkPiece(piecePos):
-                if Piece_Color==0:
-                    All_Piece.append([[piecePos[0],piecePos[1]],"black"])
-                else:
-                    All_Piece.append([[piecePos[0],piecePos[1]],"white"])
-                Piece_Color = (Piece_Color+1)%2
-        lock = pressed[0]
-
     def drawPiece(self,screen):
         '''绘制棋盘上的所有棋子'''
-        for i in All_Piece:
-            pygame.draw.circle(screen,i[1],i[0],PieceRidus)
+        for i in self.All_Piece:
+            pygame.draw.circle(screen,i[1],i[0],self.PieceRidus)
+    
+    def drawMagicPlants(self,screen):
+        '''绘制棋盘上的所有魔法植物'''
+        for i in self.Magic_plants:
+            pygame.draw.rect(screen, self.Magic_plant_Color , 
+                             [i[0]-5, i[1]-5, 5*2, 5*2])
+
+    def pixel_to_board_index(self, pixel_pos):
+        """将像素坐标转换为棋盘格坐标(row, col)"""
+        x0, y0 = self.LeftUP_Point
+        distance = self.Distance
+        x, y = int(pixel_pos[0]), int(pixel_pos[1])
+        col = round((x - x0) / distance)
+        row = round((y - y0) / distance)
+        return row, col
+
+    def syncFromPieces(self, pieces, magic_plants):
+        '''根据核心数据pieces同步棋盘显示数据All_Piece'''
+        self.All_Piece = []
+        self.Magic_plants = []
+
+        for row in range(self.Boardsize):
+            for col in range(self.Boardsize):
+                if pieces[row][col] == 1:
+                    self.All_Piece.append([[self.LeftUP_Point[0] + col * self.Distance,
+                                           self.LeftUP_Point[1] + row * self.Distance], "black"])
+                elif pieces[row][col] == -1:
+                    self.All_Piece.append([[self.LeftUP_Point[0] + col * self.Distance,
+                                           self.LeftUP_Point[1] + row * self.Distance], "white"])
+                
+                if magic_plants[row][col] == 1:
+                    self.Magic_plants.append([self.LeftUP_Point[0] + col * self.Distance,
+                                              self.LeftUP_Point[1] + row * self.Distance])
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
-from board import *
-from title_page import*
+import random
+from board import Board
+from title_page import drawTitlePage, drawWelcome
+from game_core import FourAscendGame
 
 #-------------------------------------------Initialize-----------------------------------------------
 TITLE=0;MENU=1;PVP=2;PVE=3;TEACH=4
@@ -14,12 +16,19 @@ background_image = pygame.image.load('resource\\background.png')
 titleBackground_image = pygame.image.load('resource\\titleBackground.png')
 
 
+# 游戏状态初始化
 state=TITLE
 TITLE_FLAG=True;WEL_FLAG=True
 
 board = Board()
+game = FourAscendGame()
+game_state = game.getInitBoard()
+player = 1
+#鼠标锁
+lock = 0
 
 
+# 主循环
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -27,6 +36,7 @@ while running:
 
     match state:
         case 0:
+            # 标题页和欢迎页
             if TITLE_FLAG:
                 TITLE_FLAG=drawTitlePage(screen)
             WEL_FLAG=drawWelcome(screen,titleBackground_image,WEL_FLAG)
@@ -34,13 +44,40 @@ while running:
                 if event.type==pygame.MOUSEBUTTONDOWN:
                     state = MENU
         case 1:
+            # 游戏主界面
             screen.fill(0xffffff)
             screen.blit(background_image, (0, 0))
-            board.drawUI(screen)
-            turn = board.checkMouse(screen)
-            board.setPiece(turn)
-            board.drawPiece(screen)
 
+            pieces, magic_plants, ascend_state, hp1, hp2, plant_timer = game_state
+            board.drawUI(screen, hp1, hp2, game.hp1, game.hp2)
+
+            valid = board.checkMouse(screen)
+            pressed = pygame.mouse.get_pressed()
+            # 鼠标点击落子
+            if valid and pressed[0] and not lock:
+                pixel_pos = board.findPos()
+                row, col = board.pixel_to_board_index(pixel_pos)
+                action = row * board.Boardsize + col
+                valid_moves = game.getValidMoves(game_state, player)
+
+                if 0 <= action < len(valid_moves) and valid_moves[action]:
+                    game_state, player = game.getNextState(game_state, player, action)
+                    board.syncFromPieces(game_state[0], game_state[1])
+                #鼠标上锁    
+                lock = 1
+            # 鼠标松开后解锁
+            if not pressed[0]:
+                lock = 0
+            # 绘制棋子和魔法植物    
+            board.drawPiece(screen)
+            board.drawMagicPlants(screen)
+
+            result = game.getGameEnded(game_state, player)
+            if result is not None:
+                print("Game Over:", result)
+                running = False
+            
+                
             pygame.display.flip()
     clock.tick(60)
 
