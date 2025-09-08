4a035eba62a59923e62024856ba454c76862ceec
nek0name <ylinzixuan@qq.com>
2025-09-08 22:42:16 +0800

    Add files via upload
    菜单页面+切换动画

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
 import pygame
-import board
-import background
+from board import *
+from title_page import*
+from menu_page import*
+
+#-------------------------------------------Initialize-----------------------------------------------
+TITLE=0;MENU=1;PVP=2;PVE=3;TEACH=4
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
+background_image = pygame.image.load('resource\\background.png')
+titleBackground_image = pygame.image.load('resource\\titleBackground.png')
+
+
+
+
+state=TITLE
+TITLE_FLAG=True;WEL_FLAG=True
+MENU_FLAG=True
+
+
+
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type==pygame.QUIT:
             running = False
 
-    background.drawBackGround(screen)
+    match state:
+        case 0:
+            if TITLE_FLAG:
+                TITLE_FLAG=drawTitlePage(screen)
+            WEL_FLAG=drawWelcome(screen,titleBackground_image,WEL_FLAG)
+            for event in pygame.event.get():
+                if event.type==pygame.MOUSEBUTTONDOWN:
+                    state = MENU
+        case 1:
+            if MENU_FLAG:
+                MENU_FLAG = MenuAnimation(screen,background_image)
+            drawMenuPage(screen,background_image)
+            x1=0;y1=0
+            if event.type == pygame.MOUSEMOTION:
+                x1,y1=event.pos
+            mouseJudge(screen,x1,y1)
+            # screen.fill(0xffffff)
+            # screen.blit(background_image, (0, 0))
+            # drawUI(screen)
+            # turn = checkMouse(screen)
+            # setPiece(turn)
+            # drawPiece(screen)
 
-    board.drawUI(screen)
-    turn = board.checkMouse(screen)
-    board.setPiece(turn)
-    board.drawPiece(screen)
 
-    pygame.display.flip()
+            pygame.display.flip()
 
     clock.tick(60)
 
